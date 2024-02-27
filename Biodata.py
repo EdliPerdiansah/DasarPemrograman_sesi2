6f79e92cb996caa3cfbb9e27f9fa264522e5f4a1
EdliPerdiansah <149916845+EdliPerdiansah@users.noreply.github.com>
2024-02-28 04:22:44 +0700

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,42 @@
+angkatan = 2023
+semester = 2
+fakultas = "Ilmu Komputer"
+studi = "Teknik Informatika (S1)"
+kampus = "Universitas Nusaputra"
+
+
+nama = "Edli Perdiansah"
+tempat_tanggal_lahir = "Sukabumi, 24 Maret 2002"
+jenis_kelamin = "Laki-laki"
+Agama = "Islam"
+alamat = "Cicadas Girang Rt 002/003 Kel. Baros Kota Sukabumi"
+pekerjaan = "Operator"
+no_hp = "087841652862"
+email = "edliferdiansyah1@gmail.com"
+
+print(" ")
+print("========= Welcom to Biodata =========")
+print("")
+print("========= Biodata Mahasiswa =========")
+
+print(f'''
+Nama              : {nama}
+Angkatan          : {angkatan}
+Semester Masuk    : {semester}
+Fakultas          : {fakultas}
+Program Studi     : {studi}
+Kampus            : {kampus}
+      ''')
+
+print("========= Biodata Pribadi =========")
+
+print(f'''
+Nama                    : {nama}
+Tempat tanggal Lahir    : {tempat_tanggal_lahir}
+Jenis Kelamin           : {jenis_kelamin}
+Agama                   : {Agama}
+Alamat                  : {alamat}
+Pekerjaan               : {pekerjaan}
+No Hp                   : {no_hp}
+Email                   : {email}
+      ''')
